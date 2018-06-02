7f7701f7527ce71b9f0978f51a7a9873eca91253
Leonevera <33340026+Leonevera@users.noreply.github.com>
2018-06-02 17:53:48 +0800

    Add files via upload

---
+++ Refresh/WebContent/js/header-and-footer.js
@@ -0,0 +1,16 @@
+﻿function footer_searcher() {
+    $.post("www.baidu.com", $('#search').serialize(), function (data) {
+        //data中是服务端返回的数据
+        //这里进行成功后的渲染。。。。。。
+    });
+    return false;//在这里返回false
+}
+
+function footer_change_img() {
+    var a = document.getElementById('footer-wechat');
+    a.src = "../image/wechat_2.png";
+}
+function footer_change_back_img() {
+    var a = document.getElementById('footer-wechat');
+    a.src = "../image/wechat_1.png";
+}
